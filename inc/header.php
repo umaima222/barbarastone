91b181214df865b45e4442a1136387a078872b1b
Waleed Ghani <waleed.ghani@inoviotech.local>
2022-08-09 23:03:52 +0500

    Changes

--- inc/header.php
+++ inc/header.php
@@ -1,16 +1,16 @@
 <header id="header">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-7">
+            <div class="col-md-5">
                 <div class="header-container">
-                    <div class="col-md-2 p-0 logo">
+                    <div class="col-md-3 p-0 logo">
                         <div class="logo-img">
                             <a href="index.php">
                                 <img src="img/group30.png" alt="" class="img-fluid">
                             </a>
                         </div>
                     </div>
-                    <div class="col-md-3 p-0">
+                    <div class="col-md-9 p-0">
                         <div class="side-nav">
                             <div id="nav">
                                 <ul>
@@ -22,12 +22,10 @@
                                             <li class="app"><a href="javascript:void(0);">interoable acts tyranny</a>
                                             </li>
                                             <li>
-                                                <a target="_blank"
-                                                    href="https://drive.google.com/file/d/1ShZ_X3HLM0snCu-zw7Xvjnx40zUx0G57/view">read
+                                                <a target="_blank" href="https://drive.google.com/file/d/1ShZ_X3HLM0snCu-zw7Xvjnx40zUx0G57/view">read
                                                     Our ICC Complaint</a>
                                             </li>
-                                            <li><a target="_blank"
-                                                    href="https://drive.google.com/file/d/1rR7g5cBrq5qUVTOfZhUGS5Imue2uvI0m/view">Read
+                                            <li><a target="_blank" href="https://drive.google.com/file/d/1rR7g5cBrq5qUVTOfZhUGS5Imue2uvI0m/view">Read
                                                     Our Press Release</a></li>
                                         </ul>
                                     </li>
@@ -37,17 +35,15 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-5 p-0">
+            <div class="col-md-4 p-0">
                 <div class="nav-container">
                     <div class="contact">
                         <i class='fa fa-user' style='font-size:20px ;margin-top:30px !important'></i>
                     </div>
 
                     <div class="dropdown">
-                        <a type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            <i class="fa fa-chevron-down"
-                                style="margin-left:0 ;margin-right:38px ; margin-top:35px !important; font-size:12px;"></i>
+                        <a type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-chevron-down" style="margin-left:0 ;margin-right:38px ; margin-top:35px !important; font-size:12px;"></i>
                         </a>
                         <div class="dropdown-menu     ">
                             <a class="dropdown-item" href="#">Sign in</a>
@@ -62,15 +58,16 @@
                     <div class="buttons">
                         <a href="registration-form.php"><button class="nav-btn">REGISTRATION</button></a>
                         <!--                        <a href="donation.php"><button class="nav-btn2">DONATE</button></a>-->
-                        <form action="https://www.paypal.com/donate" method="post" target="_top"><input type="hidden"
-                                name="hosted_button_id" value="RKSSV82HJHX5Y" />
-                            <input type="image" src="img/donation1.png" border="0" name="submit"
-                                title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button"
-                                style="height: 65px" />
-                            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1"
-                                height="1" />
+                        <form action="https://www.paypal.com/donate" method="post" target="_top"><input type="hidden" name="hosted_button_id" value="RKSSV82HJHX5Y" />
+                            <input type="image" src="img/donation1.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" style="height: 65px" />
+                            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
                         </form>
                     </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-0">
+                <div class="president-logo">
+                    <figure><img src="img/president-logo.png" alt="" class="img-fluid"></figure>
                 </div>
             </div>
         </div>
